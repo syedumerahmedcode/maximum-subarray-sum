c83ad1bb2875bbede6b9b95df663238bdba8efbe
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2025-06-24 21:12:37 +0200

    Feature: Create calling mechanism with happy path input array of integers.

--- src/Executor.java
+++ src/Executor.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+
+import src.main.service.MaximumSubArrayService;
+
 public class Executor {
     public static void main(String[] args) throws Exception {
         /*
@@ -10,7 +14,31 @@ public class Executor {
          * Output: 6
          * 
          */
+        generateGapInConsole();
         System.out.println(
                 "find the maximum sum of a contiguous subarray in an integer array. This is also known as Kadane's Algorithm");
+
+        MaximumSubArrayService maximumSubArrayService = new MaximumSubArrayService();
+        int[] inputToCheck = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int result = maximumSubArrayService.findMaximumSubArray(null);
+        displayResult(inputToCheck, result);
+    }
+
+    private static void displayResult(int[] inputToCheck, int result) {
+        if (result == 0) {
+            System.out.println("The maximum sum of a contiguous subarray in  the input:" + Arrays.toString(inputToCheck)
+                    + " cannot be processed correctly.");
+
+        } else {
+            System.out
+                    .println("The maximum sum of a contiguous subarray in  the input:" + Arrays.toString(inputToCheck)
+                            + " is '" + result
+                            + "'.");
+        }
+    }
+
+    private static void generateGapInConsole() {
+        System.out.println();
+        System.out.println();
     }
 }
