d46e9e47b78a7c5cfbc4ce9a004d18c9f7c02830
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2025-06-24 20:57:02 +0200

    Feature: Added skeleton classes in the project.

---
+++ src/Executor.java
@@ -0,0 +1,16 @@
+public class Executor {
+    public static void main(String[] args) throws Exception {
+        /*
+         * Write a method to find the maximum sum of a contiguous subarray in an integer
+         * array.
+         * 
+         * Please note that it is also known as Kadane's Algorithm.
+         * 
+         * int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+         * Output: 6
+         * 
+         */
+        System.out.println(
+                "find the maximum sum of a contiguous subarray in an integer array. This is also known as Kadane's Algorithm");
+    }
+}
---
+++ src/src/main/service/MaximumSubArrayService.java
@@ -0,0 +1,9 @@
+package src.main.service;
+
+public class MaximumSubArrayService {
+
+    public int findMaximumSubArray(int[] inputToCheck) {
+        return -1;
+    }
+
+}
